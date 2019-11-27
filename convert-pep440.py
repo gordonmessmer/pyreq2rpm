d63419ea3889e62d7f03481a9223e87fcda856bf
Gordon Messmer <gordon.messmer@gmail.com>
2019-11-27 07:20:35 -0800

    Add license compatible with rpm.

--- convert-pep440.py
+++ convert-pep440.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 
+# convert-pep440 - translates python packaging requirements into rpm
+# Copyright (C) 2019  Gordon Messmer <gordon.messmer@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 from pkg_resources import parse_version
 
 version_ids = ('2.4.8', '2.4.8.0', '2.4.8.1', '2.4.8.*', '2.0', '2', '2.*')
